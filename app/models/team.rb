e9134c78deb3516f7497e16091cda35c9809151e
EMILIO <12epz12@gmail.com>
2017-02-21 15:59:05 -0600

    nuevo

--- app/models/team.rb
+++
@@ -1,7 +0,0 @@
-class Team
-  include Mongoid::Document
-  field :nombre, type: String
-  field :titulos, type: Integer
-  field :seguidores, type: Integer
-  field :creacion, type: Date
-end
--- app/views/teams/_team.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! team, :id, :nombre, :titulos, :seguidores, :creacion, :created_at, :updated_at
-json.url team_url(team, format: :json)
